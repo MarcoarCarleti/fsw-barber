61dd71192d7fdb349d73add6daab2ccb325ec633
Marco Carleti <marcarleti@gmail.com>
2024-02-02 13:22:18 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,23 @@
+import { ptBR } from "date-fns/locale/pt-BR";
 import Header from "../_components/ui/header";
+import { format } from "date-fns";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl">
+          Olá, <span className="font-bold">Usuario</span>
+        </h2>
+
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+    </div>
+  );
 }
