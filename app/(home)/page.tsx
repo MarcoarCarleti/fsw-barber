b28121189839b0922052442c6555d9dd9a4d8976
MarcoarCarleti <marcarleti@gmail.com>
2024-02-03 20:53:52 -0300

    feat: changed authOptions folder

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,10 +6,11 @@ import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+
 import { Button } from "../_components/ui/button";
 import { signIn } from "next-auth/react";
 import SignInButton from "./_components/sign-in-button";
+import { authOptions } from "../_lib/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
